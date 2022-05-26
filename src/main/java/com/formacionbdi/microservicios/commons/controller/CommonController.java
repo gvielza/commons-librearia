f2c88a3aa10e3cc98a768eeb64cd684591f7e931
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-05-26 18:43:50 -0300

    agregando las validaciones @Valid

--- src/main/java/com/formacionbdi/microservicios/commons/controller/CommonController.java
+++ src/main/java/com/formacionbdi/microservicios/commons/controller/CommonController.java
@@ -1,10 +1,16 @@
 package com.formacionbdi.microservicios.commons.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,44 +18,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 import com.formacionbdi.microservicios.commons.service.CommonService;
 
-
-
-
-public class CommonController <E,S extends CommonService<E>>{
+public class CommonController<E, S extends CommonService<E>> {
 	@Autowired
 	protected S service;
 
 	@GetMapping
 	public ResponseEntity<?> listar() {
-			return ResponseEntity.ok().body(service.findAll());
+		return ResponseEntity.ok().body(service.findAll());
 	}
+
 	@GetMapping("/{id}")
-	public ResponseEntity<?> ver(@PathVariable Long id){
-		Optional<E> o=service.findById(id);
-		if(o.isEmpty()) {
+	public ResponseEntity<?> ver(@PathVariable Long id) {
+		Optional<E> o = service.findById(id);
+		if (o.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(o.get());
 	}
+//el  BindingResult result siempre debe ir siguiendo a la entidad
 	@PostMapping
-	public ResponseEntity<?> crear(@RequestBody E entity){
-		E  entityDb=service.save(entity);
+	public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+		
+		E entityDb = service.save(entity);
 		return ResponseEntity.status(HttpStatus.CREATED).body(entityDb);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> eliminar(@PathVariable Long id){
+	public ResponseEntity<?> eliminar(@PathVariable Long id) {
 		service.deleteById(id);
-	return ResponseEntity.noContent().build();
+		return ResponseEntity.noContent().build();
+	}
+	//metodo para generar los mensajes de error JSON , validaciones
+	protected ResponseEntity<?> validar(BindingResult result) {
+		Map<String, Object> errores =new HashMap<String, Object>();
+		result.getFieldErrors().forEach(err->{
+			errores.put(err.getField(), "el campo "+err.getField()+" "+err.getDefaultMessage());
+		});
+		return ResponseEntity.badRequest().body(errores);
+	}
+	
+	
+	
+	
 }
-}
-
-
-
-
-
-
-
